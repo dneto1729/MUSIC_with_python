08888f02426d4a25e71ad32c4ed99cb9c009d686
David Neto <57016287+dneto1729@users.noreply.github.com>
2024-06-11 16:35:34 -0500

    Update file
    Replace the older numpy array method with just using the awkward arrays to build the dictionary for the dataframe.

--- music_root_to_evt_strip0cut.py
+++ music_root_to_evt_strip0cut.py
@@ -1,4 +1,4 @@
-"""File to look at and generate a csv/feather file from a MUSIC events ROOT file.
+"""File to look at and generate a feather file from a MUSIC events ROOT file.
 Takes input ROOT file and process using uproot. Output file is event data along 
 row. Good events are taken from a cut on strip0 and cut on strip17.
 This is setup for the "analog DAQ" ROOT files, the newer "digital DAQ" ROOT
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 from matplotlib.colors import LogNorm
 
-import numpy as np
+# import numpy as np
 import pandas as pd
 
 FILE_NAME = ''
@@ -102,102 +102,25 @@ print('=====================================================================')
 #     + ' total events in ' + FILE_NAME)
 # print('=====================================================================')
 
+# Apply cuts as mask
 cut_run = branches[strip0_mask]
 
-# To CSV
-# file_save_name = FILE_NAME[:-5] + '_evt_strip0cut.dat'
-# print('=====================================================================')
-# print('Saving to ' + file_save_name)
-# print('=====================================================================')
-
-# with open(file_save_name, 'w', encoding='UTF-8') as f:
-#     # for i in range(0, 16000,16): # look at 1000 events
-#     for i in range(0, len(branches['strip0']), 16): # look at all "good events"
-#         f.write(str(cut_run['strip0'][i]) \
-#                 + ',' + str(cut_run['edepl'][i] + cut_run['edepr'][i]) \
-#                 + ',' + str(cut_run['edepl'][i+1] + cut_run['edepr'][i+1]) \
-#                 + ',' + str(cut_run['edepl'][i+2] + cut_run['edepr'][i+2]) \
-#                 + ',' + str(cut_run['edepl'][i+3] + cut_run['edepr'][i+3]) \
-#                 + ',' + str(cut_run['edepl'][i+4] + cut_run['edepr'][i+4]) \
-#                 + ',' + str(cut_run['edepl'][i+5] + cut_run['edepr'][i+5]) \
-#                 + ',' + str(cut_run['edepl'][i+6] + cut_run['edepr'][i+6]) \
-#                 + ',' + str(cut_run['edepl'][i+7] + cut_run['edepr'][i+7]) \
-#                 + ',' + str(cut_run['edepl'][i+8] + cut_run['edepr'][i+8]) \
-#                 + ',' + str(cut_run['edepl'][i+9] + cut_run['edepr'][i+9]) \
-#                 + ',' + str(cut_run['edepl'][i+10] + cut_run['edepr'][i+10]) \
-#                 + ',' + str(cut_run['edepl'][i+11] + cut_run['edepr'][i+11]) \
-#                 + ',' + str(cut_run['edepl'][i+12] + cut_run['edepr'][i+12]) \
-#                 + ',' + str(cut_run['edepl'][i+13] + cut_run['edepr'][i+13]) \
-#                 + ',' + str(cut_run['edepl'][i+14] + cut_run['edepr'][i+14]) \
-#                 + ',' + str(cut_run['edepl'][i+15] + cut_run['edepr'][i+15]) \
-#                 + ',' + str(cut_run['strip17'][i]) + '\n')
-
 # To DataFrame
-# First build numpy arrays for each strip
-# Hard coded for now
-print('Building Numpy Arrays')
-print('=====================================================================')
-strip_0 = np.array(cut_run['strip0'][cut_run['seg'] == 1])
-strip_1L = np.array(cut_run['edepl'][cut_run['seg'] == 1])
-strip_1R = np.array(cut_run['edepr'][cut_run['seg'] == 1])
-strip_2L = np.array(cut_run['edepl'][cut_run['seg'] == 2])
-strip_2R = np.array(cut_run['edepr'][cut_run['seg'] == 2])
-strip_3L = np.array(cut_run['edepl'][cut_run['seg'] == 3])
-strip_3R = np.array(cut_run['edepr'][cut_run['seg'] == 3])
-strip_4L = np.array(cut_run['edepl'][cut_run['seg'] == 4])
-strip_4R = np.array(cut_run['edepr'][cut_run['seg'] == 4])
-strip_5L = np.array(cut_run['edepl'][cut_run['seg'] == 5])
-strip_5R = np.array(cut_run['edepr'][cut_run['seg'] == 5])
-strip_6L = np.array(cut_run['edepl'][cut_run['seg'] == 6])
-strip_6R = np.array(cut_run['edepr'][cut_run['seg'] == 6])
-strip_7L = np.array(cut_run['edepl'][cut_run['seg'] == 7])
-strip_7R = np.array(cut_run['edepr'][cut_run['seg'] == 7])
-strip_8L = np.array(cut_run['edepl'][cut_run['seg'] == 8])
-strip_8R = np.array(cut_run['edepr'][cut_run['seg'] == 8])
-strip_9L = np.array(cut_run['edepl'][cut_run['seg'] == 9])
-strip_9R = np.array(cut_run['edepr'][cut_run['seg'] == 9])
-strip_10L = np.array(cut_run['edepl'][cut_run['seg'] == 10])
-strip_10R = np.array(cut_run['edepr'][cut_run['seg'] == 10])
-strip_11L = np.array(cut_run['edepl'][cut_run['seg'] == 11])
-strip_11R = np.array(cut_run['edepr'][cut_run['seg'] == 11])
-strip_12L = np.array(cut_run['edepl'][cut_run['seg'] == 12])
-strip_12R = np.array(cut_run['edepr'][cut_run['seg'] == 12])
-strip_13L = np.array(cut_run['edepl'][cut_run['seg'] == 13])
-strip_13R = np.array(cut_run['edepr'][cut_run['seg'] == 13])
-strip_14L = np.array(cut_run['edepl'][cut_run['seg'] == 14])
-strip_14R = np.array(cut_run['edepr'][cut_run['seg'] == 14])
-strip_15L = np.array(cut_run['edepl'][cut_run['seg'] == 15])
-strip_15R = np.array(cut_run['edepr'][cut_run['seg'] == 15])
-strip_16L = np.array(cut_run['edepl'][cut_run['seg'] == 16])
-strip_16R = np.array(cut_run['edepr'][cut_run['seg'] == 16])
-strip_17 = np.array(cut_run['strip17'][cut_run['seg'] == 1])
 
-# DEBUG
-# print('s0 ' + str(len(strip_0)))
-# print('s1L ' + str(len(strip_1L)))
-# print('s1R ' + str(len(strip_1R)))
-# print('s10L ' + str(len(strip_10L)))
-# print('s10R ' + str(len(strip_10R)))
-# print('s17 ' + str(len(strip_17)))
+dict_branches = {}
 
-# Build dataframe from numpy arrays
-# Hard coded for now
-print('Building DataFrame')
-print('=====================================================================')
-evt_d = {'s0':strip_0,'s1L':strip_1L,'s1R':strip_1R,
-         's2L':strip_2L,'s2R':strip_2R,'s3L':strip_3L,'s3R':strip_3R,
-         's4L':strip_4L,'s4R':strip_4R,'s5L':strip_5L,'s5R':strip_5R,
-         's6L':strip_6L,'s6R':strip_6R,'s7L':strip_7L,'s7R':strip_7R,
-         's8L':strip_8L,'s8R':strip_8R,'s9L':strip_9L,'s9R':strip_9R,
-         's10L':strip_10L,'s10R':strip_10R,'s11L':strip_11L,'s11R':strip_11R,
-         's12L':strip_12L,'s12R':strip_12R,'s13L':strip_13L,'s13R':strip_13R,
-         's14L':strip_14L,'s14R':strip_14R,'s15L':strip_15L,'s15R':strip_15R,
-         's16L':strip_16L,'s16R':strip_16R,'s17':strip_17
-         }
+# strip0 first
+dict_branches.update({'s0':cut_run['strip0'][cut_run['seg'] == 1]})
+# segmented anode, left first then right
+for i in range(1,17):
+    dict_branches.update({'s%iL' % i : cut_run['edepl'][cut_run['seg'] == i]})
+    dict_branches.update({'s%iR' % i  : cut_run['edepr'][cut_run['seg'] == i]})
+# strip17 last
+dict_branches.update({'s17':cut_run['strip17'][cut_run['seg'] == 1]})
 
-df = pd.DataFrame(evt_d)
+df = pd.DataFrame(dict_branches)
 
-# Save dataframe to some format
+# Save dataframe to feather file
 file_save_name = FILE_NAME[:-5] + '_evt_strip0cut.feather'
 print('Saving to ' + file_save_name)
 print('=====================================================================')
